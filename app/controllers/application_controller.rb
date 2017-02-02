7ae17ce1fcb51babde835d872a964129f01f96e7
Takaaki Minowa <takaaki-minowa@cookpad.jp>
2017-02-02 17:19:19 +0900

    Rename nothing to message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  def nothing
+  def message
+    'Hello world'
   end
 end
